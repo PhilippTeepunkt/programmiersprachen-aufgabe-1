7311347247168f81f5a68cd9bda860338abcaf20
Philipp Tornow <philipp_oeffentlich@hotmail.de>
2018-04-11 16:26:53 +0200

    implementing first method

--- source/Aufgabe1_3.cpp
+++ source/Aufgabe1_3.cpp
@@ -1,7 +1,22 @@
 #include <iostream>
 
+int kleinsterTeiler(int const& a, int const& b);
+
 int main()
 {
-    std::cout<<"hello World! \n";
+    int a;
+    int b;
+    std::cout<<"Untere Grenze eingeben:\n";
+    std::cin>>a;
+    std::cout<<"Obere Grenze eingeben:\n";
+    std::cin>>b;
+
+    std::cout<<kleinsterTeiler(a,b);
     return 0;
+}
+
+int kleinsterTeiler(int const& a, int const& b){
+
+    //Logic 
+    return a+b;
 }
